db9ef4ec63d437c14f18dd839c270808db62ce75
linguopeng <linguopeng@hyperchain.cn>
2022-07-27 11:50:05 +0800

    test: add type test

--- test/bug/bug.list.test.js
+++ test/bug/bug.list.test.js
@@ -1,100 +1,69 @@
-// const {ethers} = require("hardhat");
-//
-//
-// describe("bug", function () {
-//     this.timeout(600000)
-//
-//
-//     // 账户的第一笔交易 nonce 不能为0 =》预期可以为0
-//     it("1",async ()=>{
-//
-//     })
-//     //pending的nonce 和 latest 不一致
-//     it("0",async ()=>{
-//
-//     })
-//
-//     // 发送nonce > 当前的交易 会直接上链 =》预期在pending交易池中等待
-//     it("2",async ()=>{
-//         const testAccountAddress = await ethers.provider.getSigner(0).getAddress()
-//         console.log("test address:",testAccountAddress)
-//         // 查询账户nonce
-//         let nonce = await ethers.provider.getTransactionCount(testAccountAddress)
-//
-//
-//             // send  tx that nonce+100
-//         let sendNonce = nonce +100;
-//         try {
-//             let tx =  await ethers.provider.getSigner(0).sendTransaction( {
-//                 to: "0x0c1efcca2bcb65a532274f3ef24c044ef4ab6d73",
-//                 value: "0x1",
-//                 data:'0x',
-//                 maxFeePerGas:"0x1111",
-//                 maxPriorityFeePerGas:"0x1",
-//                 nonce:sendNonce,
-//             });
-//             let response = await tx.wait(1)
-//             console.log(response)
-//         }catch (e){
-//             return
-//         }
-//         expect("").to.be.equal("failed")
-//
-//
-//         // 再次查询nonce
-//     })
-//
-//     // 查询不存在账户的nonce 会报错 => 预期为0
-//     it("3",async ()=>{
-//
-//     })
-//
-//     // 查询不存在账户的余额 会报错=》 预期为0
-//     it("4",async ()=>{
-//
-//     })
-//     // 发送esGas 遇到非法交易 =》 没有报错
-//     it("5",async ()=>{
-//
-//     })
-//     // 发送特别多log事件 ，交易丢失，日志一直刷[2022-06-27T14:59:51.741235+08:00 ERROR jsonrpsee_core::server::helpers] Error serializing response: Error("Memory capacity exceeded", line: 0, column: 0)
-//     it("6",async ()=>{
-//         await ethers.provider.send('')
-//     })
-//
-//     //jsonRpcRequest: {
-//     //   jsonrpc: '2.0',
-//     //   method: 'eth_estimateGas',
-//     //   params: [
-//     //     {
-//     //       maxFeePerGas: '0xffff',
-//     //       maxPriorityFeePerGas: '0x1',
-//     //       from: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
-//     //       data: '0x9cb8a26a'
-//     //     }
-//     //   ],
-//     //   id: 55
-//     // }
-//     // jsonRpcResponse: { jsonrpc: '2.0', result: '0xffffffffffffffff', id: 55 }
-//     //eth_estimateGas gas is bad
-//     it("ddd",async ()=>{
-//
-//         //
-//         let contractName = "fallbackAndReceive"
-//         let contractInfo = await ethers.getContractFactory(contractName);
-//         // let nonceOfFrom = await getNonce(ethers.provider.getSigner(0).getAddress())
-//         await contractInfo.deploy({
-//             // maxFeePerGas: '0xffff',
-//             // maxPriorityFeePerGas: '0x1',
-//             // nonce: nonceOfFrom
-//         })
-//         // nonceOfFrom = await getNonce(ethers.provider.getSigner(0).getAddress())
-//         // console.log(nonceOfFrom)
-//         await contractInfo.deploy({
-//             // maxFeePerGas: '0xffff',
-//             // maxPriorityFeePerGas: '0x1',
-//             // nonce: nonceOfFrom
-//         })
-//
-//     })
-// })
+const {ethers} = require("hardhat");
+const {expect} = require("chai");
+
+
+describe("bug", function () {
+    this.timeout(600000)
+
+    it.skip("opz(reverts with invalid signature)", async () => {
+
+        // deploy contract
+        let contractInfo = await ethers.getContractFactory("ECDSAMock");
+        let contract = await contractInfo.deploy()
+        await contract.deployed()
+
+        // invoke revert
+        const TEST_MESSAGE = ethers.utils.sha256('0x1234')
+        const signature = '0x332ce75a821c982f9127538858900d87d3ec1f9f737338ad67cad133fa48feff48e6fa0c18abc62e42820f05943e47af3e9fbe306ce74d64094bdf1691ee53e01c';
+        try {
+            await contract.recover(TEST_MESSAGE, signature)
+
+        } catch (e) {
+            expect(e.toString()).to.be.include('ECDSA: invalid signature')
+            return
+        }
+        expect('').to.be.equal('failed')
+    })
+
+
+    describe('type test', function () {
+
+        it.skip('Legacy tx ,should return type 0', async () => {
+
+            // send Legacy tx
+            let tx = await ethers.provider.getSigner().sendTransaction({
+                gasPrice: "0xff"
+            })
+            let response = await tx.wait()
+
+            // Legacy tx type = 0
+            expect(response.type).to.be.equal(0)
+        })
+
+        it('1559 tx,should return type 2 ', async () => {
+
+            // send 1559 tx
+            let tx = await ethers.provider.getSigner().sendTransaction({
+                maxFeePerGas: "0x8",
+                maxPriorityFeePerGas: "0x1"
+            })
+            let response = await tx.wait()
+
+            // Legacy tx type = 0
+            expect(response.type).to.be.equal(2)
+        })
+
+        it.skip('type 2 maxPriorityFeePerGas:0x0 ,should return successful tx', async () => {
+
+            // send 1559 tx
+            let tx = await ethers.provider.getSigner().sendTransaction({
+                maxFeePerGas: "0x8",
+                maxPriorityFeePerGas: "0x0"
+            })
+            let response = await tx.wait()
+            expect(response.type).to.be.equal(2)
+        })
+    });
+
+
+})
