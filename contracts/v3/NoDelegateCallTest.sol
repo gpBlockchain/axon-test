0058bbfeb741f43ff63e9efe0980e80283751bf0
linguopeng <linguopeng@hyperchain.cn>
2022-07-27 17:42:18 +0800

    test: add v3 issue

---
+++ contracts/v3/NoDelegateCallTest.sol
@@ -0,0 +1,91 @@
+pragma solidity =0.7.5;
+
+
+
+abstract contract NoDelegateCall {
+    /// @dev The original address of this contract
+    address private immutable original;
+
+    constructor() {
+        // Immutables are computed in the init code of the contract, and then inlined into the deployed bytecode.
+        // In other words, this variable won't change when it's checked at runtime.
+        original = address(this);
+    }
+
+    /// @dev Private method is used instead of inlining into modifier because modifiers are copied into each method,
+    ///     and the use of immutable means the address bytes are copied in every place the modifier is used.
+    function checkNotDelegateCall() private view {
+        require(address(this) == original);
+    }
+
+    /// @notice Prevents delegatecall into the modified method
+    modifier noDelegateCall() {
+        checkNotDelegateCall();
+        _;
+    }
+}
+
+
+contract NoDelegateCallTest is NoDelegateCall {
+    function canBeDelegateCalled() public view returns (uint256) {
+        return block.timestamp / 5;
+    }
+
+    function cannotBeDelegateCalled() public view noDelegateCall returns (uint256) {
+        return block.timestamp / 5;
+    }
+
+    function getGasCostOfCanBeDelegateCalled() external view returns (uint256) {
+        uint256 gasBefore = gasleft();
+        canBeDelegateCalled();
+        return gasBefore - gasleft();
+    }
+
+    function getGasCostOfCannotBeDelegateCalled() external view returns (uint256) {
+        uint256 gasBefore = gasleft();
+        cannotBeDelegateCalled();
+        return gasBefore - gasleft();
+    }
+
+    function callsIntoNoDelegateCallFunction() external view {
+        noDelegateCallPrivate();
+    }
+
+    function noDelegateCallPrivate() private view noDelegateCall {}
+}
+
+
+contract CloneFactory {
+
+    mapping (address => address) public  addressMap;
+
+    function createClone(address target) public returns (address result) {
+        bytes20 targetBytes = bytes20(target);
+        assembly {
+            let clone := mload(0x40)
+            mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(add(clone, 0x14), targetBytes)
+            mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            result := create(0, clone, 0x37)
+        }
+        addressMap[target] = result;
+
+    }
+
+    function isClone(address target, address query) internal view returns (bool result) {
+        bytes20 targetBytes = bytes20(target);
+        assembly {
+            let clone := mload(0x40)
+            mstore(clone, 0x363d3d373d3d3d363d7300000000000000000000000000000000000000000000)
+            mstore(add(clone, 0xa), targetBytes)
+            mstore(add(clone, 0x1e), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+
+            let other := add(clone, 0x40)
+            extcodecopy(query, other, 0, 0x2d)
+            result := and(
+            eq(mload(clone), mload(other)),
+            eq(mload(add(clone, 0xd)), mload(add(other, 0xd)))
+            )
+        }
+    }
+}
